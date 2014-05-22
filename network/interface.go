7b4e38b76bd2b6ef798e894344ed71172bd6b217
Alex Crawford <alex.crawford@coreos.com>
2014-05-22 13:42:09 -0700

    feat(interfaces): Adding support for interfaces file
    This adds the ability for cloudinit to parse a debian interfaces
    file and generate the coresponding networkd configs. This still
    needs a lot of work to support more configuration options.

---
+++ network/interface.go
@@ -0,0 +1,216 @@
+package network
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type InterfaceGenerator interface {
+	Name() string
+	GenerateNetdevConfig() string
+	GenerateLinkConfig() string
+	GenerateNetworkConfig() string
+}
+
+type logicalInterface struct {
+	name     string
+	config   configMethod
+	children []interface{}
+}
+
+type physicalInterface struct {
+	logicalInterface
+}
+
+func (p *physicalInterface) Name() string {
+	return p.name
+}
+
+func (p *physicalInterface) GenerateNetdevConfig() string {
+	return ""
+}
+
+func (p *physicalInterface) GenerateLinkConfig() string {
+	return ""
+}
+
+func (p *physicalInterface) GenerateNetworkConfig() string {
+	config := fmt.Sprintf("[Match]\nName=%s\n\n[Network]\n", p.name)
+
+	for _, child := range p.children {
+		switch iface := child.(type) {
+		case *vlanInterface:
+			config += fmt.Sprintf("VLAN=%s\n", iface.name)
+		case *bondInterface:
+			config += fmt.Sprintf("Bond=%s\n", iface.name)
+		}
+	}
+
+	return config
+}
+
+type bondInterface struct {
+	logicalInterface
+	slaves []string
+}
+
+func (b *bondInterface) Name() string {
+	return b.name
+}
+
+func (b *bondInterface) GenerateNetdevConfig() string {
+	return fmt.Sprintf("[NetDev]\nKind=bond\nName=%s\n", b.name)
+}
+
+func (b *bondInterface) GenerateLinkConfig() string {
+	return ""
+}
+
+func (b *bondInterface) GenerateNetworkConfig() string {
+	config := fmt.Sprintf("[Match]\nName=%s\n\n[Network]\nDHCP=true\n", b.name)
+
+	for _, child := range b.children {
+		switch iface := child.(type) {
+		case *vlanInterface:
+			config += fmt.Sprintf("VLAN=%s\n", iface.name)
+		case *bondInterface:
+			config += fmt.Sprintf("Bond=%s\n", iface.name)
+		}
+	}
+
+	return config
+}
+
+type vlanInterface struct {
+	logicalInterface
+	id        int
+	rawDevice string
+}
+
+func (v *vlanInterface) Name() string {
+	return v.name
+}
+
+func (v *vlanInterface) GenerateNetdevConfig() string {
+	return fmt.Sprintf("[NetDev]\nKind=vlan\nName=%s\n\n[VLAN]\nId=%d\n", v.name, v.id)
+}
+
+func (v *vlanInterface) GenerateLinkConfig() string {
+	return ""
+}
+
+func (v *vlanInterface) GenerateNetworkConfig() string {
+	config := fmt.Sprintf("[Match]\nName=%s\n\n[Network]\n", v.name)
+	switch conf := v.config.(type) {
+	case configMethodStatic:
+		for _, nameserver := range conf.nameservers {
+			config += fmt.Sprintf("DNS=%s\n", nameserver)
+		}
+		if conf.address.IP != nil {
+			config += fmt.Sprintf("\n[Address]\nAddress=%s\n", conf.address.String())
+		}
+		for _, route := range conf.routes {
+			config += fmt.Sprintf("\n[Route]\nDestination=%s\nGateway=%s\n", route.destination.String(), route.gateway)
+		}
+	}
+
+	return config
+}
+
+func buildInterfaces(stanzas []*stanzaInterface) []InterfaceGenerator {
+	bondStanzas := make(map[string]*stanzaInterface)
+	physicalStanzas := make(map[string]*stanzaInterface)
+	vlanStanzas := make(map[string]*stanzaInterface)
+	for _, iface := range stanzas {
+		switch iface.kind {
+		case interfaceBond:
+			bondStanzas[iface.name] = iface
+		case interfacePhysical:
+			physicalStanzas[iface.name] = iface
+		case interfaceVLAN:
+			vlanStanzas[iface.name] = iface
+		}
+	}
+
+	physicals := make(map[string]*physicalInterface)
+	for _, p := range physicalStanzas {
+		if p.name == "lo" {
+			continue
+		}
+		physicals[p.name] = &physicalInterface{
+			logicalInterface{
+				name:     p.name,
+				config:   p.configMethod,
+				children: make([]interface{}, 0),
+			},
+		}
+	}
+
+	bonds := make(map[string]*bondInterface)
+	for _, b := range bondStanzas {
+		var slaves []string
+		if s, ok := b.options["bond-slaves"]; ok {
+			slaves = s
+		}
+		bonds[b.name] = &bondInterface{
+			logicalInterface{
+				name:     b.name,
+				config:   b.configMethod,
+				children: make([]interface{}, 0),
+			},
+			slaves,
+		}
+	}
+
+	vlans := make(map[string]*vlanInterface)
+	for _, v := range vlanStanzas {
+		var rawDevice string
+		id, _ := strconv.Atoi(strings.Split(v.name, ".")[1])
+		if device, ok := v.options["vlan_raw_device"]; ok && len(device) == 1 {
+			rawDevice = device[0]
+		}
+		vlans[v.name] = &vlanInterface{
+			logicalInterface{
+				name:     v.name,
+				config:   v.configMethod,
+				children: make([]interface{}, 0),
+			},
+			id,
+			rawDevice,
+		}
+	}
+
+	for _, vlan := range vlans {
+		if physical, ok := physicals[vlan.rawDevice]; ok {
+			physical.children = append(physical.children, vlan)
+		}
+		if bond, ok := bonds[vlan.rawDevice]; ok {
+			bond.children = append(bond.children, vlan)
+		}
+	}
+
+	for _, bond := range bonds {
+		for _, slave := range bond.slaves {
+			if physical, ok := physicals[slave]; ok {
+				physical.children = append(physical.children, bond)
+			}
+			if bond, ok := bonds[slave]; ok {
+				bond.children = append(bond.children, bond)
+			}
+		}
+	}
+
+	interfaces := make([]InterfaceGenerator, 0, len(physicals)+len(bonds)+len(vlans))
+	for _, physical := range physicals {
+		interfaces = append(interfaces, physical)
+	}
+	for _, bond := range bonds {
+		interfaces = append(interfaces, bond)
+	}
+	for _, vlan := range vlans {
+		interfaces = append(interfaces, vlan)
+	}
+
+	return interfaces
+}
---
+++ network/stanza.go
@@ -0,0 +1,259 @@
+package network
+
+import (
+	"fmt"
+	"net"
+	"strings"
+)
+
+type stanza interface{}
+
+type stanzaAuto struct {
+	interfaces []string
+}
+
+type stanzaInterface struct {
+	name         string
+	kind         interfaceKind
+	auto         bool
+	configMethod configMethod
+	options      map[string][]string
+}
+
+type interfaceKind int
+
+const (
+	interfaceBond = interfaceKind(iota)
+	interfacePhysical
+	interfaceVLAN
+)
+
+type route struct {
+	destination net.IPNet
+	gateway     net.IP
+}
+
+type configMethod interface{}
+
+type configMethodStatic struct {
+	address     net.IPNet
+	nameservers []net.IP
+	routes      []route
+}
+
+type configMethodLoopback struct{}
+
+type configMethodManual struct{}
+
+func parseStanzas(lines []string) (stanzas []stanza, err error) {
+	rawStanzas, err := splitStanzas(lines)
+	stanzas = make([]stanza, 0, len(rawStanzas))
+	for _, rawStanza := range rawStanzas {
+		if stanza, err := parseStanza(rawStanza); err == nil {
+			stanzas = append(stanzas, stanza)
+		} else {
+			return nil, err
+		}
+	}
+
+	autos := make([]string, 0)
+	interfaceMap := make(map[string]*stanzaInterface)
+	for _, stanza := range stanzas {
+		switch c := stanza.(type) {
+		case stanzaAuto:
+			autos = append(autos, c.interfaces...)
+		case *stanzaInterface:
+			interfaceMap[c.name] = c
+		}
+	}
+
+	// Apply the auto attribute
+	for _, auto := range autos {
+		if iface, ok := interfaceMap[auto]; ok {
+			iface.auto = true
+		}
+	}
+
+	return stanzas, nil
+}
+
+func splitStanzas(lines []string) ([][]string, error) {
+	var curStanza []string
+	stanzas := make([][]string, 0)
+	for _, line := range lines {
+		if isStanzaStart(line) {
+			if curStanza != nil {
+				stanzas = append(stanzas, curStanza)
+			}
+			curStanza = []string{line}
+		} else if curStanza != nil {
+			curStanza = append(curStanza, line)
+		} else {
+			return nil, fmt.Errorf("missing stanza start '%s'", line)
+		}
+	}
+
+	if curStanza != nil {
+		stanzas = append(stanzas, curStanza)
+	}
+
+	return stanzas, nil
+}
+
+func isStanzaStart(line string) bool {
+	switch strings.Split(line, " ")[0] {
+	case "auto":
+		fallthrough
+	case "iface":
+		fallthrough
+	case "mapping":
+		return true
+	}
+
+	if strings.HasPrefix(line, "allow-") {
+		return true
+	}
+
+	return false
+}
+
+func parseStanza(rawStanza []string) (stanza, error) {
+	if len(rawStanza) == 0 {
+		panic("empty stanza")
+	}
+	tokens := strings.Fields(rawStanza[0])
+	if len(tokens) < 2 {
+		return nil, fmt.Errorf("malformed stanza start '%s'", rawStanza[0])
+	}
+
+	kind := tokens[0]
+	attributes := tokens[1:]
+
+	switch kind {
+	case "auto":
+		return parseAutoStanza(attributes, rawStanza[1:])
+	case "iface":
+		return parseInterfaceStanza(attributes, rawStanza[1:])
+	default:
+		return nil, fmt.Errorf("unknown stanza '%s'", kind)
+	}
+}
+
+func parseAutoStanza(attributes []string, options []string) (stanzaAuto, error) {
+	return stanzaAuto{interfaces: attributes}, nil
+}
+
+func parseInterfaceStanza(attributes []string, options []string) (*stanzaInterface, error) {
+	if len(attributes) != 3 {
+		return nil, InterfaceMissingAttributesError(fmt.Errorf("incorrect number of attributes"))
+	}
+
+	iface := attributes[0]
+	confMethod := attributes[2]
+
+	optionMap := make(map[string][]string, 0)
+	for _, option := range options {
+		if strings.HasPrefix(option, "post-up") {
+			tokens := strings.SplitAfterN(option, " ", 2)
+			if v, ok := optionMap["post-up"]; ok {
+				optionMap["post-up"] = append(v, tokens[1])
+			} else {
+				optionMap["post-up"] = []string{tokens[1]}
+			}
+		} else if strings.HasPrefix(option, "pre-down") {
+			tokens := strings.SplitAfterN(option, " ", 2)
+			if v, ok := optionMap["pre-down"]; ok {
+				optionMap["pre-down"] = append(v, tokens[1])
+			} else {
+				optionMap["pre-down"] = []string{tokens[1]}
+			}
+		} else {
+			tokens := strings.Fields(option)
+			optionMap[tokens[0]] = tokens[1:]
+		}
+	}
+
+	iface := attributes[0]
+	//addressFamily := attributes[1]
+	confMethod := attributes[2]
+
+	var conf configMethod
+	switch confMethod {
+	case "static":
+		config := configMethodStatic{
+			routes:      make([]route, 0),
+			nameservers: make([]net.IP, 0),
+		}
+		if addresses, ok := optionMap["address"]; ok {
+			if len(addresses) == 1 {
+				config.address.IP = net.ParseIP(addresses[0])
+			}
+		}
+		if netmasks, ok := optionMap["netmask"]; ok {
+			if len(netmasks) == 1 {
+				config.address.Mask = net.IPMask(net.ParseIP(netmasks[0]))
+			}
+		}
+		if config.address.IP == nil || config.address.Mask == nil {
+			return nil, fmt.Errorf("malformed static network config for '%s'", iface)
+		}
+		if gateways, ok := optionMap["gateway"]; ok {
+			if len(gateways) == 1 {
+				config.routes = append(config.routes, route{
+					destination: net.IPNet{
+						IP:   net.IPv4(0, 0, 0, 0),
+						Mask: net.IPv4Mask(0, 0, 0, 0),
+					},
+					gateway: net.ParseIP(gateways[0]),
+				})
+			}
+		}
+		for _, nameserver := range optionMap["dns-nameservers"] {
+			config.nameservers = append(config.nameservers, net.ParseIP(nameserver))
+		}
+		for _, postup := range optionMap["post-up"] {
+			if strings.HasPrefix(postup, "route add") {
+				route := route{}
+				fields := strings.Fields(postup)
+				for i, field := range fields {
+					switch field {
+					case "-net":
+						route.destination.IP = net.ParseIP(fields[i+1])
+					case "netmask":
+						route.destination.Mask = net.IPMask(net.ParseIP(fields[i+1]))
+					case "gw":
+						route.gateway = net.ParseIP(fields[i+1])
+					}
+				}
+				config.routes = append(config.routes, route)
+			}
+		}
+		conf = config
+	case "loopback":
+		conf = configMethodLoopback{}
+	case "manual":
+		conf = configMethodManual{}
+	}
+
+	if strings.Contains(iface, ".") {
+		return parseVLANStanza(iface, conf, attributes, optionMap)
+	}
+
+	if _, ok := optionMap["bond-slaves"]; ok {
+		return parseBondStanza(iface, conf, attributes, optionMap)
+	}
+
+	return parsePhysicalStanza(iface, conf, attributes, optionMap)
+}
+
+func parseBondStanza(iface string, conf configMethod, attributes []string, options map[string][]string) (*stanzaInterface, error) {
+	return &stanzaInterface{name: iface, kind: interfaceBond, configMethod: conf, options: options}, nil
+}
+
+func parsePhysicalStanza(iface string, conf configMethod, attributes []string, options map[string][]string) (*stanzaInterface, error) {
+	return &stanzaInterface{name: iface, kind: interfacePhysical, configMethod: conf, options: options}, nil
+}
+
+func parseVLANStanza(iface string, conf configMethod, attributes []string, options map[string][]string) (*stanzaInterface, error) {
+	return &stanzaInterface{name: iface, kind: interfaceVLAN, configMethod: conf, options: options}, nil
+}
